a51f69263a815399a7286a6068e03bb632f8c536
TimMcN <timothyjmcnamara@gmail.com>
2023-12-20 18:59:19 +0000

    Delimter string split working

--- Day2/Day2.cpp
+++ Day2/Day2.cpp
@@ -8,23 +8,31 @@ std::vector<std::string> split_string(std::string input, std::string delimeter)
     std::vector<std::string> tokens;
     std::string::iterator pos = input.begin();
     pos = input.begin();
-
-    while(input.find(delimeter))
+    while(input.find(delimeter)!=std::string::npos)
     {
-        tokens.push_back(pos, input.substr(input.find(delimeter)));
-        pos = input.begin()+input.find(delimeter);
-        input = std::string(pos, input.end());
+        pos = input.begin()+input.find(delimeter)+1;
+        std::string token = std::string(input.begin(), pos-1);
+        if (token!=delimeter && token != " " && token != "")
+        {
+            tokens.push_back(token);
+        }
+         input = std::string(pos, input.end());
+    }
+    if (input.size() >0)
+    {
+        tokens.push_back(input);
     }
     return tokens;
-
-
 }
 int main()
 {
-    std::string test = "Testing space as a delimeter";
-    std::vector<std::string> result = split_string(test, " ");
+    std::string test = ",Testing,Commas,as,Delimeters,";
+    std::vector<std::string> result = split_string(test, ",");
+    int i =0;
+    std::cout<< result.size()<<std::endl;
     for (std::vector<std::string>::iterator pos = result.begin(); pos<result.end(); pos++)
-    {   
-        std::cout<<*pos << std::endl;
+    {
+        std::cout<<i << ": " << *pos <<std::endl;
+        i++;
     }
 }
