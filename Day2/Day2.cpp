a99069a2846452eb0a55b619255d9cd8db270c41
TimMcN <timothyjmcnamara@gmail.com>
2023-12-22 15:49:30 +0000

    Solved part1 & 2, removed unnecessary prints

--- Day2/Day2.cpp
+++ Day2/Day2.cpp
@@ -2,7 +2,33 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
 
+int pospow(int b, int p)
+{
+    if (p == 0)
+    {
+        return 1;
+    }
+    else{
+        return b*(pospow (b, p-1));
+    }
+}
+std::vector<std::string> readfile(std::string filepath)
+{
+    std::vector<std::string> filecontent;
+    std::ifstream ifile(filepath);
+    if (ifile.is_open())
+    {
+        std::string line;
+        while (std::getline(ifile, line))
+        {
+            filecontent.push_back(line);
+        }
+    }
+    ifile.close();
+    return filecontent;
+}
 std::vector<std::string> split_string(std::string input, std::string delimeter)
 {
     std::vector<std::string> tokens;
@@ -24,6 +50,45 @@ std::vector<std::string> split_string(std::string input, std::string delimeter)
     }
     return tokens;
 }
+std::string remove_char(std::string input, char delimeter)
+{
+    std::string::iterator pos = input.begin();
+    std::string output;
+    for (pos; pos<input.end(); pos++)
+    {
+        if(*pos != delimeter)
+        {
+            output+=*pos;
+        }
+    }
+    return output;
+}
+std::string replace_char(std::string input, char target, char replacement)
+{
+    std::string::iterator pos = input.begin();
+    std::string output;
+    for (pos; pos<input.end(); pos++)
+    {
+        if(*pos!= target){
+            output+=*pos;
+        }
+        else{
+            output+=replacement;
+        }
+    }
+    return output;
+}
+int string_to_int(std::string stringnum)
+{
+    int output =0;
+    int size = stringnum.size();
+    for (std::string::iterator pos = stringnum.begin(); pos<stringnum.end(); pos++)
+    {
+        output = output + (int(*pos-'0')) * pospow(10, size-1);
+        size--;
+    }
+    return output;
+}
 void tester(std::string input, std::string delimeter)
 {
     std::vector<std::string> result = split_string(input, delimeter);
@@ -31,14 +96,60 @@ void tester(std::string input, std::string delimeter)
     std::cout<< result.size()<<std::endl;
     for (std::vector<std::string>::iterator pos = result.begin(); pos<result.end(); pos++)
     {
-        std::cout<<i << ": " << *pos <<std::endl;
         i++;
     }
 
 }
 int main()
 {
-    tester(",testing,commas,as,delimeters,", ",");
-    tester("Imagine a world where I could write a sentence and it would give me back the words", " ");
-    tester("does the . work as a delimeter", ".");
+    std::vector<std::string> raw_input = readfile("/home/tim/Desktop/AOC/AdventOfCodeSolutions/Day2/input");
+    std::vector<int> possible_games;
+    std::map<std::string, int> max_colour = {
+        {"red", 12},
+        {"green", 13},
+        {"blue", 14}
+    };
+    for (std::vector<std::string>::iterator line=raw_input.begin(); line<raw_input.end(); line++)
+    {
+        int game_id= string_to_int(*(split_string(*((split_string(*line, ":")).begin()), " ").end()-1));
+        std::string games = *((split_string(*line, ":")).end()-1);
+        games = replace_char(games, ';', ',');
+        std::vector<std::string> draws = split_string(games, ",");
+        for (std::vector<std::string>::iterator pos = draws.begin(); pos<draws.end(); pos++)
+        {
+            if (max_colour[*(split_string(*pos, " ").begin()+1)] < string_to_int(*(split_string(*pos, " ").begin())))
+            {
+                game_id = 0;
+            }
+        }
+        possible_games.push_back(game_id);
+    }
+    int count=0;
+    for (std::vector<int>::iterator loc= possible_games.begin(); loc < possible_games.end(); loc++)
+    {
+        count += *loc;
+    } 
+    std::cout<<"COUNT: " << count << std::endl;
+    /* Part 2 */
+    count =0;
+    for (std::vector<std::string>::iterator line=raw_input.begin(); line<raw_input.end(); line++)
+        {
+            max_colour["red"] = 0;
+            max_colour["green"] = 0;
+            max_colour["blue"] = 0;
+
+            std::string games = *((split_string(*line, ":")).end()-1);
+            games = replace_char(games, ';', ',');
+            std::vector<std::string> draws = split_string(games, ",");
+            for (std::vector<std::string>::iterator pos = draws.begin(); pos<draws.end(); pos++)
+            {
+                if (max_colour[*(split_string(*pos, " ").begin()+1)] < string_to_int(*(split_string(*pos, " ").begin())))
+                {
+                    max_colour[*(split_string(*pos, " ").begin()+1)] = string_to_int(*(split_string(*pos, " ").begin()));
+                }
+            }
+            count += max_colour["red"]*max_colour["green"]*max_colour["blue"];
+        }
+        std::cout<<"COUNT: " << count << std::endl;
+      
 }
